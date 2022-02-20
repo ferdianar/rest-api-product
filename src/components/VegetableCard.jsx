66174943cce0bcb9891d8c2a991533873d14ba14
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-02-20 15:11:21 +0700

    initial commit and add tailwind config

---
+++ src/components/VegetableCard.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const VegetableCard = (props) => {
+       const { product, deleteItems } = props
+       return (
+              <React.Fragment>
+                     {
+                            product.map(productItems => (
+                                   <div key={productItems.id} className="w-full flex flex-col md:flex-row justify-between p-3 rounded-md bg-gray-900 text-white">
+                                          <div className="">
+                                                 <h3 className="font-bold text-sm">{productItems.title}</h3>
+                                                 <p className="text-xs text-yellow-300 font-normal mt-1">{productItems.description}</p>
+                                                 <small>{productItems.location}</small>
+                                          </div>
+                                          <div className="flex flex-col gap-y-2 md:mt-0 mt-4">
+                                                 <button className="py-1 px-3 font-medium text-sm text-white bg-green-600 hover:bg-green-700 rounded-md">Edit</button>
+                                                 <button onClick={() => deleteItems(productItems.id)} className="py-1 px-3 font-medium text-sm text-white bg-red-600 hover:bg-red-700 rounded-md">Delete</button>
+                                          </div>
+                                   </div>
+                            ))
+                     }
+              </React.Fragment >
+       )
+}
+
+export default VegetableCard
+
+/**
+ *const VegetableCard = (props) => {
+       return (
+              <React.Fragment>
+                     {
+                            props.product.map(productItems => (
+                                   <div className="max-w-[250px] flex justify-between p-3 my-2 rounded-md bg-gray-800 text-white">
+                                          <div className="">
+                                                 <h3 key={productItems.id} className="font-bold text-sm">{productItems.title}</h3>
+                                                 <p className="text-xs text-white font-normal">{productItems.description}</p>
+                                                 <small>{productItems.location}</small>
+                                          </div>
+                                          <div className="flex flex-col gap-y-2">
+                                                 <button className="py-1 px-3 font-medium text-sm text-white bg-green-600 hover:bg-green-700 rounded-md">Edit</button>
+                                                 <button className="py-1 px-3 font-medium text-sm text-white bg-red-600 hover:bg-red-700 rounded-md">Delete</button>
+                                          </div>
+                                   </div>
+                            ))
+                     }
+              </React.Fragment>
+       )
+}
+ */
